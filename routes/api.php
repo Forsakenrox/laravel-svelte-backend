faa93c6c110cfd0406c2bd193e1f83b9a027345a
ROX <ROX31@yandex.ru>
2022-11-14 04:38:17 +0300

    ew

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProfileCOntroller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->group(function () {
     // sleep(1);
     Route::resource('posts', PostController::class);
+    Route::get('profile', [ProfileCOntroller::class, 'index']);
 });
 Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
